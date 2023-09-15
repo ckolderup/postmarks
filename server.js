bbdc36c52e3fa81abfeff4d79c31ef923173b1bd
Casey Kolderup <ckolderup@fastly.com>
2023-09-15 10:42:01 -0700

    Revert "should fail github actions check with 63 errors"
    This reverts commit 34dba68b98d709b76ffd99f8cc5f9db29cb4c71b.

--- server.js
+++ server.js
@@ -1,120 +1,120 @@
-import * as dotenv from 'dotenv'
-import express from 'express'
-import cors from 'cors'
-import { create } from 'express-handlebars'
+import * as dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import { create } from 'express-handlebars';
 
-import { domain, account, simpleLogger, actorInfo } from './src/util.js'
-import session, { isAuthenticated } from './src/session-auth.js'
-import * as bookmarksDb from './src/bookmarks-db.js'
-import * as apDb from './src/activity-pub-db.js'
+import { domain, account, simpleLogger, actorInfo } from './src/util.js';
+import session, { isAuthenticated } from './src/session-auth.js';
+import * as bookmarksDb from './src/bookmarks-db.js';
+import * as apDb from './src/activity-pub-db.js';
 
-import routes from './src/routes/index.js'
+import routes from './src/routes/index.js';
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-const app = express()
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(express.json({ type: 'application/activity+json' }))
-app.use(session())
+const app = express();
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.json({ type: 'application/activity+json' }));
+app.use(session());
 
 app.use((req, res, next) => {
-  res.locals.loggedIn = req.session.loggedIn
-  return next()
-})
+  res.locals.loggedIn = req.session.loggedIn;
+  return next();
+});
 
-app.set('site_name', actorInfo.displayName || 'Postmarks')
-app.set('bookmarksDb', bookmarksDb)
-app.set('apDb', apDb)
-app.set('account', account)
-app.set('domain', domain)
+app.set('site_name', actorInfo.displayName || 'Postmarks');
+app.set('bookmarksDb', bookmarksDb);
+app.set('apDb', apDb);
+app.set('account', account);
+app.set('domain', domain);
 
-app.disable('x-powered-by')
+app.disable('x-powered-by');
 
 // force HTTPS in production
 if (process.env.ENVIRONMENT === 'production') {
-  app.set('trust proxy', ['127.0.0.1', '10.0.0.0/8'])
+  app.set('trust proxy', ['127.0.0.1', '10.0.0.0/8']);
 
   app.use(({ secure, hostname, url, port }, response, next) => {
     if (!secure) {
-      return response.redirect(308, `https://${hostname}${url}${port ? `:${port}` : ''}`)
+      return response.redirect(308, `https://${hostname}${url}${port ? `:${port}` : ''}`);
     }
 
-    return next()
-  })
+    return next();
+  });
 } else {
-  console.log("ENVIRONMENT is not 'production', HTTPS not forced")
+  console.log("ENVIRONMENT is not 'production', HTTPS not forced");
 }
 
 const hbs = create({
   helpers: {
     pluralize(number, singular, plural) {
-      if (number === 1) return singular
-      return typeof plural === 'string' ? plural : `${singular}s`
+      if (number === 1) return singular;
+      return typeof plural === 'string' ? plural : `${singular}s`;
     },
     htmlize(text) {
       // uh-oh. ohhhh no.
-      return text?.replace('\n', '<br/>')
+      return text?.replace('\n', '<br/>');
     },
     siteName() {
-      return app.get('site_name')
+      return app.get('site_name');
     },
     account() {
-      return app.get('account')
+      return app.get('account');
     },
     feedLink() {
-      return `<link rel="alternate" type="application/atom+xml" href="https://${app.get('domain')}/index.xml" />`
+      return `<link rel="alternate" type="application/atom+xml" href="https://${app.get('domain')}/index.xml" />`;
     },
     projectUrl() {
-      return `https://${app.get('domain')}`
+      return `https://${app.get('domain')}`;
     },
     glitchProjectName() {
-      return process.env.PROJECT_DOMAIN
+      return process.env.PROJECT_DOMAIN;
     },
     section(name, options) {
-      if (!this._sections) this._sections = {}
-      this._sections[name] = options.fn(this)
-      return null
+      if (!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
     },
     mastodonAccount() {
-      return process.env.MASTODON_ACCOUNT
+      return process.env.MASTODON_ACCOUNT;
     },
     ifIn(item, array, options) {
-      return array.indexOf(item) >= 0 ? options.fn(this) : options.inverse(this)
+      return array.indexOf(item) >= 0 ? options.fn(this) : options.inverse(this);
     },
     removeTag(tag, path) {
       return path
         .split('/')
         .filter((x) => x !== tag)
-        .join('/')
+        .join('/');
     },
     ifThisTag(tag, path, options) {
-      return path === `/tagged/${tag}` ? options.fn(this) : options.inverse(this)
+      return path === `/tagged/${tag}` ? options.fn(this) : options.inverse(this);
     },
   },
   partialsDir: './src/pages/partials',
   extname: '.hbs',
-})
+});
 
-app.set('view engine', '.hbs')
-app.set('views', './src/pages')
-app.engine('.hbs', hbs.engine)
+app.set('view engine', '.hbs');
+app.set('views', './src/pages');
+app.engine('.hbs', hbs.engine);
 
-app.use(simpleLogger)
+app.use(simpleLogger);
 
-app.use('/admin', isAuthenticated, routes.admin)
-app.use('/', routes.auth)
-app.use('/bookmark', routes.bookmark)
-app.use('/comment', routes.comment)
-app.use('/.well-known/webfinger', cors(), routes.webfinger)
-app.use('/u', cors(), routes.user)
-app.use('/m', cors(), routes.message)
-app.use('/', routes.core)
-app.use('/api/inbox', cors(), routes.inbox)
-app.use('/.well-known/nodeinfo', routes.nodeinfo)
-app.use('/nodeinfo/2.0', routes.nodeinfo)
+app.use('/admin', isAuthenticated, routes.admin);
+app.use('/', routes.auth);
+app.use('/bookmark', routes.bookmark);
+app.use('/comment', routes.comment);
+app.use('/.well-known/webfinger', cors(), routes.webfinger);
+app.use('/u', cors(), routes.user);
+app.use('/m', cors(), routes.message);
+app.use('/', routes.core);
+app.use('/api/inbox', cors(), routes.inbox);
+app.use('/.well-known/nodeinfo', routes.nodeinfo);
+app.use('/nodeinfo/2.0', routes.nodeinfo);
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
