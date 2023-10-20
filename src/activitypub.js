c0e620927a533760d4cc107a96d641a0650ff1c9
Jeff Sikes <jeffsikes@users.noreply.github.com>
2023-10-20 10:44:35 -0500

    Hashtag spacing fixes

--- src/activitypub.js
+++ src/activitypub.js
@@ -34,13 +34,17 @@ export function createNoteObject(bookmark, account, domain) {
   updatedBookmark.title = escapeHTML(bookmark.title);
   updatedBookmark.description = escapeHTML(bookmark.description);
 
-  const linkedTags = bookmark.tags
+  let linkedTags = bookmark.tags
     ?.split(' ')
     .map((tag) => {
       const tagName = tag.slice(1);
-      return `<a href="https://${domain}/tagged/${tagName}" class="mention hashtag" rel="tag nofollow noopener noreferrer">${tag}</a>`;
+      return `<a href="https://${domain}/tagged/${tagName}" class="mention hashtag" rel="tag nofollow noopener noreferrer">${tag}<</a>`;
     })
     .join(' ');
+
+  if (linkedTags?.trim().length > 0) {
+    linkedTags = `<p>${linkedTags}</p>`;
+  }
 
   const noteMessage = {
     '@context': 'https://www.w3.org/ns/activitystreams',
@@ -48,10 +52,10 @@ export function createNoteObject(bookmark, account, domain) {
     type: 'Note',
     published: d.toISOString(),
     attributedTo: `https://${domain}/u/${account}`,
-    content: `<strong><a href="${updatedBookmark.url}" rel="nofollow noopener noreferrer" target="_blank">${replaceEmptyText(
+    content: `<p><strong><a href="${updatedBookmark.url}" rel="nofollow noopener noreferrer">${replaceEmptyText(
       updatedBookmark.title,
       updatedBookmark.url,
-    )}</a></strong><br/>${updatedBookmark.description?.trim().replace('\n', '<br/>') || ''}<p>${linkedTags}</p>`,
+    )}</a></strong><br/>${updatedBookmark.description?.trim().replace('\n', '<br/>') || ''}</p>${linkedTags}`,
     to: [`https://${domain}/u/${account}/followers/`, 'https://www.w3.org/ns/activitystreams#Public'],
     tag: [],
   };
