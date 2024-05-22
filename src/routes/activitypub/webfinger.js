addde702b2b989b7fd666cd9df4792b080636393
Steve Bate <svc-github@stevebate.net>
2024-05-22 12:19:37 +0200

    Changes to make WebFinger RFC-conformant * Use `application/jrd+json` content type for JRD responses. * Verify resource matches Postmarks account. * Use 404 status for unsupported resource URI scheme.

--- src/routes/activitypub/webfinger.js
+++ src/routes/activitypub/webfinger.js
@@ -5,16 +5,17 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   const { resource } = req.query;
   if (!resource || !resource.includes('acct:')) {
-    return res.status(400).send('Bad request. Please make sure "acct:USER@DOMAIN" is what you are sending as the "resource" query parameter.');
+    return res.status(404).send('Not found. Please make sure "acct:USER@DOMAIN" is what you are sending as the "resource" query parameter.');
   }
 
   const name = resource.replace('acct:', '');
   const db = req.app.get('apDb');
   const webfinger = await db.getWebfinger();
-  if (webfinger === undefined) {
+  if (webfinger === undefined || webfinger.subject !== resource) {
     return res.status(404).send(`No webfinger record found for ${name}.`);
   }
 
+  res.setHeader('content-type', 'application/jrd+json');
   return res.json(JSON.parse(webfinger));
 });
 
